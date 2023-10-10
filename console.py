0b9e5a4bf61f9596283d7cfc1c438f4db2894579
Mally13 <mallymutuku13@gmail.com>
2023-10-10 09:58:29 +0300

    Fix: empty line and unknown command

--- console.py
+++ console.py
@@ -1,8 +1,35 @@
+#!/usr/bin/python3
+
 import cmd
 import sys
 
-class MyConsole(cmd.Cmd):
-    pass
+class HbnbConsole(cmd.Cmd):
+    """
+    Defines the HBnB command intepreter
+    """
+    prompt = "(hbnb) "
 
-if __name__  = "__main__"
-    MyConsole().cmdloop()
+    def default(self, line):
+        """
+        Called for unknown commands
+        """
+        if not line.strip():
+            pass
+        else:
+            print("*** Unknown syntax: {}".format(line))
+
+    def emptyline(self):
+        """Does nothing in case of an empty input"""
+        pass
+
+    def do_create(self, line):
+        print("I have created", line)
+
+    def do_quit(self, line):
+        return True
+
+if __name__  == "__main__":
+    try:
+        HbnbConsole().cmdloop()  
+    except KeyboardInterrupt:
+        print("\n Exiting...")
