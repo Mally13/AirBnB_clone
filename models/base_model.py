c9347179b1607daa3dfdb59194cf7722484effd9
kyalo3 <kyalokimeu0@gmail.com>
2023-10-11 11:19:24 +0300

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,5 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 
-import uuid
-from datetime import datetime
-
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())  # Generate a unique ID as a string
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
-    def save(self):
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        obj_dict = self.__dict__.copy()  # Create a copy of the instance's __dict__
-
-        # Convert created_at and updated_at to ISO format strings
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-
-        # Add the __class__ key with the class name
-        obj_dict['__class__'] = self.__class__.__name__
-
-        return obj_dict
-=======
 """Defines BaseModel class"""
 import uuid
 import datetime
@@ -79,4 +51,3 @@ class BaseModel():
     def __str__(self):
         class_name = self.__class__.__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}"
->>>>>>> main
