bad646dd6ceeaa9162edcbdd896ed01fdc81d689
kyalo3 <kyalokimeu0@gmail.com>
2023-10-15 08:42:36 +0300

    pycodestyle errors

--- models/amenity.py
+++ models/amenity.py
@@ -6,5 +6,6 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """ a class Amenity that inherits from BaseModel"""
     name = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -6,5 +6,6 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ a class State that inherits from BaseModel"""
     name = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **args)
