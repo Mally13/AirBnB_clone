738a39978b7b44bc6509bc2b3a293600c70e620a
kyalo3 <kyalokimeu0@gmail.com>
2023-10-16 01:19:20 +0300

    pycodestyle

--- models/user.py
+++ models/user.py
@@ -6,10 +6,10 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """ a class User that inherits from BaseModel"""
 
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
