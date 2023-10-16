b38ff9cdbf9f0e73449f90a254c2969583727cd2
kyalo3 <kyalokimeu0@gmail.com>
2023-10-16 13:56:23 +0300

    sample files on deserializing

---
+++ date_time.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import json
+import datetime
+
+time_now = datetime.datetime.now()
+
+print(time_now)
---
+++ deserialization.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import json
+
+students = {
+    "id" : "877",
+    "name" : "Kyalo"
+}
+
+student_json = json.dumps(students)
+
+student_dict = json.loads(student_json)
+print(student_dict)
